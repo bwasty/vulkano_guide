afe2c20d80802c04a314ef9095d7d58475f0c94f
Benjamin Wasty <benny.wasty@gmail.com>
2018-08-18 13:10:09 +0200

    Compute operations
    http://vulkano.rs/guide/compute-intro
    http://vulkano.rs/guide/compute-pipeline
    http://vulkano.rs/guide/descriptor-sets
    http://vulkano.rs/guide/dispatch

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 #![allow(unused_variables)]
 
 use std::sync::Arc;
+use std::time::Instant;
 
+#[allow(unused_imports)]
 #[macro_use]
 extern crate vulkano;
+
+#[macro_use]
+extern crate vulkano_shader_derive;
 
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
@@ -19,6 +24,12 @@ use vulkano::buffer::BufferUsage;
 use vulkano::buffer::CpuAccessibleBuffer;
 
 use vulkano::command_buffer::AutoCommandBufferBuilder;
+use vulkano::command_buffer::CommandBuffer;
+
+use vulkano::sync::GpuFuture;
+
+mod utils;
+use utils::print_elapsed;
 
 struct MyStruct {
     a: u32,
@@ -51,37 +62,6 @@ fn initialize() -> (Arc<Device>, Arc<Queue>) {
     (device, queue)
 }
 
-fn main() {
-    let (device, queue) = initialize();
-
-    buffer_creation(device.clone());
-
-    // http://vulkano.rs/guide/example-operation
-    let source_content = 0 .. 64;
-    let source = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
-                                                source_content).expect("failed to create buffer");
-
-    let dest_content = (0 .. 64).map(|_| 0);
-    let dest = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
-                                              dest_content).expect("failed to create buffer");
-
-    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap()
-        .copy_buffer(source.clone(), dest.clone()).unwrap()
-        .build().unwrap();
-
-    use vulkano::command_buffer::CommandBuffer;
-    let finished = command_buffer.execute(queue.clone()).unwrap();
-
-    use vulkano::sync::GpuFuture;
-
-    finished.then_signal_fence_and_flush().unwrap()
-        .wait(None).unwrap();
-
-    let src_content = source.read().unwrap();
-    let dest_content = dest.read().unwrap();
-    assert_eq!(&*src_content, &*dest_content);
-}
-
 // http://vulkano.rs/guide/buffer-creation
 fn buffer_creation(device: Arc<Device>) {
     let data = 12;
@@ -105,4 +85,102 @@ fn buffer_creation(device: Arc<Device>) {
     // `content` implements `DerefMut` whose target is of type `MyStruct` (the content of the buffer)
     content.a *= 2;
     content.b = false;
+}
+
+// http://vulkano.rs/guide/example-operation
+fn example_operation_copy(device: Arc<Device>, queue: Arc<Queue>) {
+    let source_content = 0 .. 64;
+    let source = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+                                                source_content).expect("failed to create buffer");
+
+    let dest_content = (0 .. 64).map(|_| 0);
+    let dest = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+                                              dest_content).expect("failed to create buffer");
+
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap()
+        .copy_buffer(source.clone(), dest.clone()).unwrap()
+        .build().unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+
+    finished.then_signal_fence_and_flush().unwrap()
+        .wait(None).unwrap();
+
+    let src_content = source.read().unwrap();
+    let dest_content = dest.read().unwrap();
+    assert_eq!(&*src_content, &*dest_content);
+}
+
+fn main() {
+    let (device, queue) = initialize();
+
+    buffer_creation(device.clone());
+
+    example_operation_copy(device.clone(), queue.clone());
+
+    // http://vulkano.rs/guide/compute-intro
+    let timer = Instant::now();
+
+    let data_iter = 0 .. 65536;
+    let data_buffer = CpuAccessibleBuffer::from_iter(device.clone(), BufferUsage::all(),
+                                                     data_iter).expect("failed to create buffer");
+
+    let shader = cs::Shader::load(device.clone())
+        .expect("failed to create shader module");
+
+
+    use vulkano::pipeline::ComputePipeline;
+
+    let compute_pipeline = Arc::new(ComputePipeline::new(device.clone(), &shader.main_entry_point(), &())
+        .expect("failed to create compute pipeline"));
+
+
+    use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
+
+    let set = Arc::new(PersistentDescriptorSet::start(compute_pipeline.clone(), 0)
+        .add_buffer(data_buffer.clone()).unwrap()
+        .build().unwrap()
+    );
+
+    let command_buffer = AutoCommandBufferBuilder::new(device.clone(), queue.family()).unwrap()
+        .dispatch([1024, 1, 1], compute_pipeline.clone(), set.clone(), ()).unwrap()
+        .build().unwrap();
+
+    let finished = command_buffer.execute(queue.clone()).unwrap();
+    print_elapsed("set up compute and execute", timer);
+
+    let timer = Instant::now();
+    finished.then_signal_fence_and_flush().unwrap()
+        .wait(None).unwrap();
+    print_elapsed("wait for compute execution", timer);
+    let timer = Instant::now();
+
+    let content = data_buffer.read().unwrap();
+    for (n, val) in content.iter().enumerate() {
+        assert_eq!(*val, n as u32 * 12);
+    }
+    print_elapsed("read back result", timer);
+
+    println!("Everything succeeded!");
+}
+
+#[allow(dead_code)]
+mod cs {
+    #[derive(VulkanoShader)]
+    #[ty = "compute"]
+    #[src = "
+#version 450
+
+layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+
+layout(set = 0, binding = 0) buffer Data {
+    uint data[];
+} buf;
+
+void main() {
+    uint idx = gl_GlobalInvocationID.x;
+    buf.data[idx] *= 12;
+}"
+    ]
+    struct Dummy;
 }
---
+++ src/utils.rs
@@ -0,0 +1,34 @@
+/// Copied from https://github.com/bwasty/gltf-viewer/blob/master/src/utils.rs
+
+use std::time::{ Instant, Duration };
+
+pub fn elapsed(start_time: Instant) -> String {
+    let elapsed = start_time.elapsed();
+    format_duration(elapsed)
+}
+
+fn format_duration(duration: Duration) -> String {
+    let secs = duration.as_secs();
+    let nanos = duration.subsec_nanos();
+    let ms = f64::from(nanos) / 1_000_000.0;
+    if secs > 0 {
+        let secs = secs as f64 + ms / 1000.0;
+        format!("{:<4.*} s", 1, secs)
+    }
+    else {
+        let places =
+            if ms >= 20.0      { 0 }
+            else if ms >= 1.0  { 1 }
+            else {
+                let micros = f64::from(nanos) / 1000.0;
+                let places = if micros >= 10.0 { 0 } else { 2 };
+                return format!("{:>3.*} µs", places, micros)
+            };
+        format!("{:>3.*} ms", places, ms)
+    }
+}
+
+#[allow(dead_code)]
+pub fn print_elapsed(message: &str, start_time: Instant) {
+    println!("{:<30}{}", message, elapsed(start_time));
+}
